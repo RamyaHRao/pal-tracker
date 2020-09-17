34579a1e1c84eee5252d299abdf223eb33bf2e2d
ubuntu <ramyaraoh@gmail.com>
2020-09-17 18:21:32 +0000

    Spring JDBC Template

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -1,7 +1,10 @@
 package io.pivotal.pal.tracker;
+        import com.mysql.cj.jdbc.MysqlDataSource;
         import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
         import org.springframework.context.annotation.Bean;
+        import javax.sql.DataSource;
+        import javax.xml.crypto.Data;
 
 @SpringBootApplication
 public class PalTrackerApplication{
@@ -11,9 +14,17 @@ public class PalTrackerApplication{
     }
 
     @Bean
-     public TimeEntryRepository timeEntryRepository(){
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource){
+
+
+        JdbcTimeEntryRepository jdbcTimeEntryRepository = new JdbcTimeEntryRepository(dataSource);
+
+        return jdbcTimeEntryRepository;
+    }
+
+     /*public TimeEntryRepository timeEntryRepository(){
 
         return  new InMemoryTimeEntryRepository();
-    }
+    }*/
 }
 
